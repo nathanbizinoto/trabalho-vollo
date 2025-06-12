b24c3fc68a34a618e6e239b831c46ab8663d47e6
nathanbizinotto <nathan.oliveira@cloudwalk.io>
2025-06-12 19:22:21 -0300

    Fix: Corrigir configuração de portas - aplicação agora roda consistentemente na porta 8182

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN npm install
 
 COPY . .
 
-EXPOSE 3000
+EXPOSE 8182
 
 CMD ["npm", "start"] 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8182;
 
 // Configuração do banco de dados
 const pool = new Pool({
